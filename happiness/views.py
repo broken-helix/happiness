edb4c8602a21bca12340566e631e046b5351893c
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-03-22 17:42:40 +0000

    Added form view

--- happiness/views.py
+++ happiness/views.py
@@ -1,10 +1,41 @@
 from django.views.generic import TemplateView
-
-
-class HomeView(TemplateView):
-    template_name = 'index.html'
-
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views import generic
+from .forms import PostForm
+from .models import Post
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+
+class AddPostPage(LoginRequiredMixin, generic.CreateView):
+    """
+    Allows a logged-in user to create a new blog post.
+
+    Methods:
+    - form_valid: Overrides the base method to set the post author to the
+      currently logged-in user before saving the form.
+
+    Mixins:
+    - LoginRequiredMixin: Ensures that only authenticated users can access
+      this view.
+    """
+
+    form_class = PostForm
+    template_name = "form.html"
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        response = super().form_valid(form)
+        messages.success(
+            self.request,
+            "Post created successfully! Review in progress!")
+        return response
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        messages.error(
+            self.request,
+            "Post creation failed. Please check your input.")
+        return response
